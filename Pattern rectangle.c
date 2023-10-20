35b81e4eb1a5f1edd34b37b818e99311b5c9ec5c
Sandip Maity <145787637+Sandip-Maity-2023@users.noreply.github.com>
2023-10-20 17:53:23 +0530

    Create Pattern rectangle.c

---
+++ Pattern rectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int row=5;
+for(int i=0;i<row;i++)
+{
+for(int j=0;j<row;j++)
+{
+if(i==0 || i==row-1 || j==0 || j==row-1)
+printf("*");
+else
+printf(" ");
+}
+printf("\n");
+}
+}
