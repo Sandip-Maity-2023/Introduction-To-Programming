dc20e97e07289748bfc95baba7c3969422b2f0ac
CODING DECONG <12sandip125@gmail.com>
2024-01-05 08:45:56 +0530

    .

---
+++ 97 preprocessor directive in c Macros addition and substraction.c
@@ -0,0 +1,12 @@
+//
+// Created by 12san on 05-01-2024.
+//
+#include<stdio.h>
+#define a 10
+int main(){
+    int b=20,c=30;
+    printf("%d\n",b+c+a);
+    printf("%d\n",b-a);
+    printf("%d",a-b);
+    return 0;
+}
