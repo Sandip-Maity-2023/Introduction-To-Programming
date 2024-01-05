b0ec0c6a9b295c8a6d7a38aeb63cd7a733ba6d1d
CODING DECONG <12sandip125@gmail.com>
2024-01-05 09:22:38 +0530

    PI value of preprocessor directive
    PI=3.1416.........
    #define

---
+++ 99 c Preprocessor directive of PI.c
@@ -0,0 +1,12 @@
+//
+// Created by 12san on 05-01-2024.
+//
+#include<stdio.h>
+#define PI 3.14167891
+int main(){
+    float r;
+    printf("Enter the radius of circle:");
+    scanf("%f",&r);
+    printf("The area of circle is:%f",PI*r*r);
+    return 0;
+}
