b5e453df217f6998fdae1ea6cd5ca05dacd8b54d
CODING DECONG <12sandip125@gmail.com>
2024-01-23 18:49:13 +0530

    project

---
+++ 150 Project of Introduction-To-Programming.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<ourheaderfile.h>
+int main(){
+    int x,y;
+    int addition;
+    printf("Enter two number:");
+    scanf("%d",&x &y);
+    printf("%d",addition(x,y));
+    return 0;
+}
---
+++ ourheaderfile.h
@@ -0,0 +1,15 @@
+int addition(int x,int y){
+    return x+y;
+}
+int substraction(int x,int y){
+    return x-y;
+}
+int multiplication(int x,int y){
+    return x*y;
+}
+int addition(int x,int y){
+    return x/y;
+}
+int modulus(int x,int y){
+    return x%y;
+}
