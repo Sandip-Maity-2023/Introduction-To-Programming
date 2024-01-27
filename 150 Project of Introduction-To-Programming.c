166ab9c3fa2ff8b42710d11fe6bb9c40a131bd4e
CODING DECONG <12sandip125@gmail.com>
2024-01-27 23:20:53 +0530

    clion

--- 150 Project of Introduction-To-Programming.c
+++ 150 Project of Introduction-To-Programming.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 #include"ourheaderfile.h"
 int main(){
-    int a,b;
+    int a,b,z,x,y,num;
+    printf("Choose the option :\t1.Armstrong number\t2.Area of a square\t3.Radius of a circle\t4.ASCII value\t5.calculation:\n");
+    scanf("%d",&z);
+    switch(z){
+        case 1:
+        printf("Enter number to check Armstrong numbers:");
+        scanf("%d",&x);
+        if(armstrong(num)==0){
+        printf("%d is an armstrong number.",num);
+        }else{
+            printf("%d is not an armstrong number.",num);
+        }
+        break;
+        case 2:    
     printf("Enter two number:\n");
     scanf("%d %d",&a, &b);
     printf("Addition is: %d\n",sum(a,b));
@@ -9,5 +22,9 @@ int main(){
     printf("Multiplication is: %d\n",sum(a,b));
     printf("Division is: %d\n",sum(a,b));
     printf("Modulus is: %d\n",sum(a,b));
-    return 0;
+    break;
+ default:
+        printf("Sorry unavailable.....");
+        break;
+}
 }
--- ourheaderfile.h
+++ ourheaderfile.h
@@ -13,3 +13,19 @@ int divide(int x,int y){
 int mod(int x,int y){
     return x%y;
 }
+int armstrong(int num){
+    int lastdigit=0;
+    int sum=0;
+    int power=0;
+    int n=num;
+    while(n!=0){
+        lastdigit=n%10;
+        power=lastdigit*lastdigit*lastdigit;
+        sum=sum+power;
+        n=n/10;
+    }
+    if(sum=num)
+      return 0;
+    else
+      return 1;
+}
