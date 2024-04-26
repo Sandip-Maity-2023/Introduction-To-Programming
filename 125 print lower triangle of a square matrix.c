d1221f7ce90cf122354727f820728f3fbbb1be15
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-04-26 22:32:11 +0530

    NPTEL & CLion

--- 125 print lower triangle of a square matrix.c
+++ 125 print lower triangle of a square matrix.c
@@ -2,3 +2,24 @@
 // Created by 12san on 22-04-2024.
 //
 #include<stdio.h>
+int main(){
+    int matrix[20][20];
+    int r;
+    printf("Enter the number of rows and columns:\n");
+    scanf("%d",&r);
+    for(int i=0;i<r;i++){
+        for(int j=0;j<r;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<r;j++){
+            if(i>=j)
+                printf("%d",matrix[i][j]);
+            else
+                printf("%d",0);
+        }
+        printf("\n");
+    }
+    return 0;
+}
