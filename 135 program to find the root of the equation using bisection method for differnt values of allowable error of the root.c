8bfc175efa6a0ec2d031daed82d3db03d0070747
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-05-11 00:37:34 +0530

    VS code & CLion

--- 135 program to find the root of the equation using bisection method for differnt values of allowable error of the root.c
+++ 135 program to find the root of the equation using bisection method for differnt values of allowable error of the root.c
@@ -1,3 +1,42 @@
-//
-// Created by 12san on 28-04-2024.
-//
+#include<stdio.h>
+#include<ctype.h>
+
+float fun(float x);
+void bisection(float *x, float a, float b, int *itr);
+
+int main(){
+    int itr = 0, maxmitr = 10;
+    float x, a = 1.0, b = 2.0, allerr, x1;
+
+    printf("Enter the error tolerance: ");
+    scanf("%f", &allerr);
+
+    bisection(&x, a, b, &itr);
+
+    do {
+        if(fun(a) * fun(x) < 0)
+            b = x;
+        else
+            a = x;
+
+        bisection(&x1, a, b, &itr);
+
+        if((x1 - x) < allerr) {
+            printf("Root = %1.4f\n", x1);
+            return 0;
+        }
+
+        x = x1;
+    } while(itr < maxmitr);
+
+    return 1;
+}
+
+float fun(float x){
+    return (2 * x * x * x - 3 * x - 5);
+}
+
+void bisection(float *x, float a, float b, int *itr){
+    *x = (a + b) / 2;
+    ++(*itr);
+}
