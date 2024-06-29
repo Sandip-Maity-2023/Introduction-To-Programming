5a1ca9fcddbd904410acd5fb8ab0ff7ce702b8a4
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-06-29 20:05:53 +0530

    patterns with small adjustments

---
+++ p2.c
@@ -0,0 +1,18 @@
+//
+// Created by 12san on 29-06-2024.
+//
+#include<stdio.h>
+int main(){
+    int n=4,z=1;
+    for(int i=1;i<=n;i++){
+        for(int j=(n-1);j>=i;j--){
+            printf(" ");
+        }
+        for(int k=1;k<=z;k++){
+            printf("%d",i);
+        }
+        z+=2;
+        printf("\n");
+    }
+    return 0;
+}
---
+++ p5.c
@@ -0,0 +1,21 @@
+//
+// Created by 12san on 29-06-2024.
+//
+//
+// Created by 12san on 29-06-2024.
+//
+#include<stdio.h>
+int main(){
+    int n=4,z=1;
+    for(int i=1;i<=n;i++){
+        for(int j=(n-1);j>=i;j--){
+            printf(" ");
+        }
+        for(int k=z;k>=1;k--){
+            printf("%d",k);
+        }
+        z+=2;
+        printf("\n");
+    }
+    return 0;
+}
