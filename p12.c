050d2e3eedef64c44c990e76c8a6926ede941b51
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-07-24 18:13:31 +0530

    ..

--- p12.c
+++ p12.c
@@ -1,10 +1,11 @@
 /*
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
 */
+
 #include<stdio.h>
 int main(){
     int n;
@@ -12,7 +13,7 @@ int main(){
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            printf("%d ",j);
+            printf("%d ",i);
         }
         printf("\n");
     }
--- p13.c
+++ p13.c
@@ -1,9 +1,9 @@
 /*
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
+#
+# #
+# # #
+# # # #
+# # # # #
 */
 #include<stdio.h>
 int main(){
@@ -11,8 +11,8 @@ int main(){
     printf("Enter the value of n:\n");
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            printf("%d ",j);
+        for(int j=1;j<=i;j++){
+            printf("# ");
         }
         printf("\n");
     }
