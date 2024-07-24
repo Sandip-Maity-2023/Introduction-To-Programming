7c7acc61f29d6efb732b36a72acb06dea5ebfe6e
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-07-24 17:41:01 +0530

    ..

---
+++ p11.c
@@ -0,0 +1,19 @@
+/*
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the value of n:\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+}
