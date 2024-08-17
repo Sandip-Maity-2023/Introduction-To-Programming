e00e8c6981789e18c2db1b510ae2c9b435f631e5
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-08-17 22:57:04 +0530

    practice

---
+++ PROJECT 07 Bank Management system.c
@@ -0,0 +1,5 @@
+//
+// Created by 12san on 17-08-2024.
+//
+
+#include "PROJECT 07 Bank Management system.h"
---
+++ PROJECT 08 Inventory Management system.c
@@ -0,0 +1,5 @@
+//
+// Created by 12san on 17-08-2024.
+//
+
+#include "PROJECT 08 Inventory Management system.h"
