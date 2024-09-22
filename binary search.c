61dabde59ef8dec4a73be0be57b173c2d7812c49
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-09-23 00:48:03 +0530

    ,,

--- binary search.c
+++ binary search.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int key = 12;
+    int n, search;
 
-    int low = 0;
-    int high = size - 1;
+
+    printf("Enter the size of the array:\n");
+    scanf("%d", &n);
+
+    int arr[n];
+
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search:\n");
+    scanf("%d", &search);
+
+    int first = 0;
+    int last = n - 1;
     int mid;
     int found = 0;
 
-    while (low <= high) {
-        mid = low + (high - low) / 2;
 
-        // Check if the key is present at the middle
-        if (arr[mid] == key) {
+    while (first <= last) {
+        mid = (first + last) / 2;
+
+        if (arr[mid] == search) {
+            printf("Element %d is found at location %d.\n", search, mid + 1);
             found = 1;
             break;
-        }
-
-        // If key is greater, ignore the left half
-        else if (arr[mid] < key) {
-            low = mid + 1;
-        }
-
-        // If key is smaller, ignore the right half
-        else {
-            high = mid - 1;
+        } else if (arr[mid] < search) {
+            first = mid + 1;
+        } else {
+            last = mid - 1;
         }
     }
 
-    if (found) {
-        printf("Element %d found at index %d\n", key, mid);
-    } else {
-        printf("Element %d not found in the array\n", key);
+    if (!found) {
+        printf("Element %d does not exist.\n", search);
     }
 
     return 0;
