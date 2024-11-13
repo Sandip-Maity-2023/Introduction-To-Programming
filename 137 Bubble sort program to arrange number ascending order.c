84cc8e29f81267ba323050b9d6fb1cbf7dc0c692
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-11-13 22:22:00 +0530

    update

--- 137 Bubble sort program to arrange number ascending order.c
+++ 137 Bubble sort program to arrange number ascending order.c
@@ -1,16 +1,22 @@
 //
 // Created by 12san on 14-05-2024.
 //
+
+
+problems...................................
 #include<stdio.h>
 void sort(int n,int *a);
 int main() {
     int n, a[20];
     printf("Enter the size:");
     scanf("%d", &n);
+
+    printf("Enter the elements:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
     sort(n, a);
+    printf("Sorted elements are:\t");
     for (int i = 0; i < n; i++) {
         printf("%d\n", a[i]);
     }
