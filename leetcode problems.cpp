eada5fdad646f84e9f1aacc06fd58ca96d5fe2b6
Sandip-Maity-2023 <12sandip125@gmail.com>
2025-01-18 01:11:35 +0530

    update

---
+++ leetcode problems.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 12san on 10-01-2025.
+//
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main() {
+    int l1=0,l2=0,str2;
+    const char *str1;
+    cout<<"Enter str1:\n";
+    cin>>str1;
+    cout<<"Enter str2:\n";
+    cin>>str2;
+    l1=strlen(str1);
+    
+    l2=strlen(str2);
+
+
+
+    return 0;
+}
