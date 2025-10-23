363e2b4a19002c723b00fc3b2b8696e8013e95c7
Sandip-Maity-2023 <12sandip125@gmail.com>
2025-10-23 14:33:51 +0530

    up

--- 208 4merge sort.c
+++ 208 4merge sort.c
@@ -2,60 +2,65 @@
 // Created by 12san on 20-12-2024.
 //
 #include<stdio.h>
+
 #define size 100
-int merge(int arr[],int beg,int mid,int end){
-    int i=beg,j=mid+1,index=beg,temp[size];
-    while(i<=mid && j<=end){
-if(arr[i]<arr[j]){
-    temp[index]=arr[i];
-    i++;
-}else{
-    temp[index]=arr[j];
-    j++;
-}
-index++;
+
+int merge(int arr[], int l, int m, int r) {
+    int i = l, j = m + 1, index = l, temp[size];
+
+    while (i <= m && j <= r) {
+        if (arr[i] < arr[j]) {
+            temp[index] = arr[i];
+            i++;
+        } else {
+            temp[index] = arr[j];
+            j++;
+        }
+        index++;
     }
-    if(i>mid){
-        while(j<=end){
-            temp[index]=arr[j];
+    if (i > m) {
+        while (j <= r) {
+            temp[index] = arr[j];
             j++;
             index++;
         }
-    }else{  //here i<=mid
-        while(i<=mid){
-            temp[index]=arr[i];
+    } else {  //here i<=mid
+        while (i <= m) {
+            temp[index] = arr[i];
             i++;
             index++;
         }
     }
-    for(int i=beg;i<index;i++){
-        arr[i]=temp[i];
+    for (int i = l; i < index; i++) {
+        arr[i] = temp[i];
     }
 }
-void merge_sort(int arr[],int beg,int end){
-    int mid;
-    if(beg<end){
-        mid=(beg+end)/2;
-        merge_sort(arr,beg,mid);
-        merge_sort(arr,mid+1,end);
-        merge(arr,beg,mid,end);
+
+void merge_sort(int arr[], int l, int r) {
+    int m;
+    if (l < r) {
+        m = (l + r) / 2;
+        merge_sort(arr, l, m);
+        merge_sort(arr, m+ 1, r);
+        merge(arr, l, m, r);
     }
 }
-int main(){
+
+int main() {
     int n;
     printf("Enter no of elements:\n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
     printf("Enter the elements:\n");
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    for(int i=0;i<n;i++){
-        printf("%d\t\a",arr[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\a", arr[i]);
     }
     printf("\n");
-    merge_sort(arr,0,n-1);
-    for(int i=0;i<n;i++){
-        printf("%d\t\a",arr[i]);
+    merge_sort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\a", arr[i]);
     }
 }
